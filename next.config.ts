04b489b4f819b0a55b1bdc3f5103a53609825e86
God-Coding <humanbull9986@gmail.com>
2025-12-09 16:24:36 +0530

    Configure Next.js to allow PayU origin for Server Actions

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,11 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  experimental: {
+    serverActions: {
+      allowedOrigins: ['ssfashions.vercel.app', 'secure.payu.in'],
+    },
+  },
 };
 
 export default nextConfig;
